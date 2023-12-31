ec1560e914c697e41355f9107697d47211eef890
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-30 21:00:33 -0800

    Update DeleteHack.bat

--- DeleteHack.bat
+++ DeleteHack.bat
@@ -1,6 +1,6 @@
 setlocal > Hack_ip_tools.exe
 endloacl
-echo >> Thanks-for-use > endHack_ip.txt
+echo  Thanks-for-use > endHack_ip.txt
 start endHack_ip.txt
 start https://pastebin.com/raw/uGP0DmUn
 exit
